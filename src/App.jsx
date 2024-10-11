c56995a994e21ed9bf5992da324bc6be1545c669
William Mercês <engenheirowilliammerces@gmail.com>
2024-10-11 09:44:44 -0500

    Atualizacion de Carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
 
-import { Route, Routes } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
 import SearchBar from './coponents/SearchBar'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
+import Home from './pages/home/HomePage';
 
 export default function App() {
 
   return (
-
-
-    <>
-
-
+    <> <Router>
       <Routes>
-
-        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/' element= {<Home />} />
         <Route path='/todos' element={<h1>Todos</h1>} />
         <Route path='/posts' element={<h1>Posts</h1>} />
-       
-       
         <Route path='/albums' element={
           <>
             <div className='container  h-screen overflow-hidden'>
-
               <SearchBar />
-
             </div>
-
           </>} />
-
-       
-
+      </Routes>
+    </Router>
     </>
   )
 }
