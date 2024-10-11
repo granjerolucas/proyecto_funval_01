7556ec90ff3dd3cf0e6bac1ae3f475cf97e7dba7
JcReyes29 <j.creyesp29@gmail.com>
2024-10-11 12:37:26 -0500

    union-componentes

--- src/pages/albums/AlbumPage.jsx
+++ src/pages/albums/AlbumPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
+import Card from './components/Card'
 
 export default function AlbumPage() {
   return (
-    <div className='container  h-screen overflow-hidden'>
-        
-    <SearchBar
-    />
-  </div>
+    <div className='container h-screen overflow-hidden mx-auto'>
+      <SearchBar
+      />
+      <Card />
+    </div>
 
   )
 }
