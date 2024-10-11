cbddcd8b1fed9cd90b3cd7524a35db34488918ef
JcReyes29 <j.creyesp29@gmail.com>
2024-10-11 14:46:30 -0500

    layout-album-page

--- src/pages/albums/AlbumPage.jsx
+++ src/pages/albums/AlbumPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
 import Card from './components/Card'
+import Layout from '../../components/Layout'
 
 export default function AlbumPage() {
   return (
-    <div className='container h-screen overflow-hidden mx-auto'>
-      <SearchBar
-      />
-      <Card />
-    </div>
+    <Layout>
+      <div className='container h-screen overflow-hidden mx-auto'>
+        <SearchBar
+        />
+        <Card />
+      </div>
+    </Layout>
+
 
   )
 }
